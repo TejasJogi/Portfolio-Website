298fe58a200ad96db9abf3d54225fb07290e6b7f
TejasJogi <tejasjogi1995@gmail.com>
2022-06-25 17:33:30 +0530

    smtp config added

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+from email import message
 from flask import Flask, render_template, request
 from flask_mail import Mail, Message
+from config import mail_username, mail_passwoerd
 
 
 app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = "smtp.gmail.com"
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = mail_username
+app.config['MAIL_password'] = mail_passwoerd
+
 
 mail = Mail(app)
 
@@ -19,9 +29,7 @@ def about():
 @app.route('/contact', methods=['GET','POST'])
 def contact():
     if request.method == "POST":
-        return "Sent Message"
+        message = "sent message"
     return render_template('contact.html')
-
-
 if __name__ == '__main__':
    app.run(debug = True)
